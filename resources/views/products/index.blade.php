c8c5b967a142a91ebb3af0fe57236a6ed425bab9
Miquel Mariño Espinosa <miquel@MacBook-Air-de-Miquel.local>
2023-09-20 17:15:00 +0200

    product delete

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -23,6 +23,7 @@
                 <th>Price</th>
                 <th>Description</th>
                 <th>Edit</th>
+                <th>Delete</th>
             </tr>
             @foreach ($products as $product)
                 <tr>
@@ -33,6 +34,13 @@
                     <td>{{$product->description}}</td>
                     <td>
                         <a href="{{route('product.edit', ['product' => $product])}}">Edit</a>
+                    </td>
+                    <td>
+                        <form method="post" action="{{route('product.delete', ['product' => $product])}}">
+                            @csrf
+                            @method('delete')
+                            <input type="submit" value="Delete" />
+                        </form>
                     </td>
                 </tr>
             @endforeach
